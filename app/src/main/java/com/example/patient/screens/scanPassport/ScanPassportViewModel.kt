2fe4bfc447d1e7260994d21fd2ad317dd18b3a80
bunyod <b.bekmurodov@alif.mobi>
2022-01-23 08:59:52 +0500

    small fix

--- app/src/main/java/com/example/patient/screens/scanPassport/ScanPassportViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.patient.screens.scanPassport
-
-import com.example.patient.utils.base.BaseViewModel
-
-class ScanPassportViewModel : BaseViewModel() {
-    // TODO: Implement the ViewModel
-}
