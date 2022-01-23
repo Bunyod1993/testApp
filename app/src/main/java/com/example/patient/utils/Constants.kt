7f9b8de7f1274e1c518ccd51ffd100065ce5e2ac
bunyod <b.bekmurodov@alif.mobi>
2022-01-23 13:34:50 +0500

    to be continued

--- app/src/main/java/com/example/patient/utils/Constants.kt
+++ app/src/main/java/com/example/patient/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.patient.utils
 
 object Constants {
-    const val BASE_URL = "https://apitestc.alif.tj"
+    const val BASE_URL = "https://stackoverflow.com"
     const val STACKOVERFLOW_API_KEY = "ZiXCZbWaOwnDgpVT9Hx8IA(("
     const val AUTH_TOKEN = "authToken"
 //    const val RUS_FORMAT = "+7 ### ### ## ##"
