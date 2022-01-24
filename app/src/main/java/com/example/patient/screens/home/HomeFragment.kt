f3e1f1a733e49d3586df1cb1d5121c4c72aa9047
bunyod <b.bekmurodov@alif.mobi>
2022-01-24 09:13:26 +0500

    3 screns ready

--- app/src/main/java/com/example/patient/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/patient/screens/home/HomeFragment.kt
@@ -1,11 +1,7 @@
 package com.example.patient.screens.home
 
 
-import android.graphics.drawable.Drawable
 import android.os.CountDownTimer
-import androidx.core.content.res.ResourcesCompat
-import androidx.navigation.Navigation
-import com.example.patient.R
 import com.example.patient.databinding.HomeFragmentBinding
 import com.example.patient.utils.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,38 +16,11 @@ class HomeFragment : BaseFragment<HomeFragmentBinding, HomeViewModel>() {
     override fun observeData() {
         super.observeData()
 
-        val endIcon: Drawable? = ResourcesCompat.getDrawable(context!!.resources,
-                                R.drawable.ic_refresh, null)
-
-        timer = object : CountDownTimer(3000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-
-            }
-
-            override fun onFinish() {
-                binding.otpFieldLayout.endIconDrawable=endIcon
-            }
-        }
-
-
     }
 
     override fun setUpViews() {
         super.setUpViews()
-//        could be done dynamic placeholder
-//        binding.otpFieldLayout.placeholderText=""
-        binding.sendOtp.setOnClickListener {
-//            startTimer()
-//            Navigation.findNavController(it).navigate(R.id.action_homeToScanPassport)
-        }
 
-        binding.otpFieldLayout.setEndIconOnClickListener {
-            startTimer()
-        }
     }
 
-    private fun startTimer() {
-      timer.start()
-      binding.otpFieldLayout.endIconDrawable=null
-    }
 }
