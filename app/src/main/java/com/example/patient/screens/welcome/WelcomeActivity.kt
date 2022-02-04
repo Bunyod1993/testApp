19d2271f963860be5f3ef0f816ad5e59d8f50dd9
bunyod <b.bekmurodov@alif.mobi>
2022-02-04 16:43:45 +0500

    small fix

--- app/src/main/java/com/example/patient/screens/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/patient/screens/welcome/WelcomeActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import androidx.core.view.WindowCompat
 import com.example.patient.R
 import com.example.patient.adapters.WelcomePagerAdapter
 import com.example.patient.databinding.ActivityMainBinding
@@ -15,10 +16,6 @@ class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(binding.root)
     }
 }
