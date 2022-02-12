00dd5d8b6dc5c075bffd928e887f5408450f0ca6
bunyod <b.bekmurodov@alif.mobi>
2022-02-12 16:02:13 +0500

    3 screens ready

--- app/src/main/java/com/example/patient/screens/localPatients/LocalPatientsFragment.kt
+++ app/src/main/java/com/example/patient/screens/localPatients/LocalPatientsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.patient.screens.localPatients
 
 import com.example.patient.databinding.LocalPatientsFragmentBinding
+import com.example.patient.screens.MainActivity
 import com.example.patient.utils.base.BaseFragment
 
 class LocalPatientsFragment : BaseFragment<LocalPatientsFragmentBinding, LocalPatientsViewModel>() {
@@ -9,6 +10,8 @@ class LocalPatientsFragment : BaseFragment<LocalPatientsFragmentBinding, LocalPa
     override fun getViewModelClass() = LocalPatientsViewModel::class.java
     override fun setUpViews() {
         super.setUpViews()
+        (activity as MainActivity).setSupportActionBar(binding.toolbar)
+
 
     }
 
