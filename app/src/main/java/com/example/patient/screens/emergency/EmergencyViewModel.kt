f45f639c3515c58ddcf66f56af04d675edfef4d4
bunyod <b.bekmurodov@alif.mobi>
2022-02-13 11:24:31 +0500

    reverse fragment also ready

--- app/src/main/java/com/example/patient/screens/emergency/EmergencyViewModel.kt
+++ app/src/main/java/com/example/patient/screens/emergency/EmergencyViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.patient.screens.emergency
 
 import com.example.patient.utils.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class EmergencyViewModel : BaseViewModel() {
+@HiltViewModel
+class EmergencyViewModel @Inject constructor(): BaseViewModel() {
     // TODO: Implement the ViewModel
 }
--- app/src/main/java/com/example/patient/screens/reverse/ReverseRegisterViewModel.kt
+++ app/src/main/java/com/example/patient/screens/reverse/ReverseRegisterViewModel.kt
@@ -2,8 +2,9 @@ package com.example.patient.screens.reverse
 
 import com.example.patient.utils.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class ReverseRegisterViewModel : BaseViewModel() {
+class ReverseRegisterViewModel @Inject constructor() : BaseViewModel() {
     // TODO: Implement the ViewModel
 }
