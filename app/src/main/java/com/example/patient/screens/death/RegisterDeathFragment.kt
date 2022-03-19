343c019f1bfe9a6d20433b99a69b788846d35654
bunyod <b.bekmurodov@alif.mobi>
2022-03-19 10:04:46 +0500

    reason screen ready

--- app/src/main/java/com/example/patient/screens/death/RegisterDeathFragment.kt
+++ app/src/main/java/com/example/patient/screens/death/RegisterDeathFragment.kt
@@ -1,10 +1,19 @@
 package com.example.patient.screens.death
 
+import androidx.lifecycle.lifecycleScope
 import com.example.patient.databinding.RegisterDeathFragmentBinding
 import com.example.patient.screens.MainActivity
 import com.example.patient.utils.base.BaseFragment
+import com.example.patient.utils.ui.invisible
+import com.example.patient.utils.ui.reset
+import com.example.patient.utils.ui.validate
+import com.example.patient.utils.ui.visible
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@FlowPreview
 @AndroidEntryPoint
 class RegisterDeathFragment : BaseFragment<RegisterDeathFragmentBinding, RegisterDeathViewModel>() {
 
@@ -15,6 +24,86 @@ class RegisterDeathFragment : BaseFragment<RegisterDeathFragmentBinding, Registe
         super.setUpViews()
         (activity as MainActivity).setSupportActionBar(binding.toolbar)
         binding.viewModel = viewModel
+        binding.checkbox1.setOnCheckedChangeListener { _, b ->
+            if (b) {
+                binding.firstDeathReasonWrapper.visible()
+                viewModel.setNumberOfFields(2)
+                binding.checkbox2.isChecked = false
+            } else binding.firstDeathReasonWrapper.invisible()
+        }
+        binding.checkbox2.setOnCheckedChangeListener { _, b ->
+            if (b) {
+                viewModel.setNumberOfFields(3)
+                binding.secondDeathReasonWrapper.visible()
+                binding.checkbox1.isChecked = false
+            } else binding.secondDeathReasonWrapper.invisible()
+        }
+        binding.deathRegionField.setOnFocusChangeListener { _, b ->
+            if (b) {
+                viewModel.validateDeathRegionOne()
+            }
+        }
+        binding.deathRegion2.setOnFocusChangeListener { _, b ->
+            if (b) {
+                viewModel.validateDeathRegionTwo()
+            }
+        }
+        binding.deathReason.setOnFocusChangeListener { _, b ->
+            if (b) {
+                viewModel.validateDeathReasonOne()
+            }
+        }
+        binding.deathReason2.setOnFocusChangeListener { _, b ->
+            if (b) {
+                viewModel.validateDeathReasonTwo()
+            }
+        }
+        binding.deathHours.setOnFocusChangeListener { _, b ->
+            if (b) {
+                viewModel.validateDeathHours()
+            }
+        }
+    }
+
+    override fun observeData() {
+        super.observeData()
+        viewModel.deathHours.observe(viewLifecycleOwner){
+            binding.deathHours.reset()
+        }
+        viewModel.deathReasonOne.observe(viewLifecycleOwner){
+            binding.deathReason.reset()
+        }
+        viewModel.deathReasonTwo.observe(viewLifecycleOwner){
+            binding.deathReason2.reset()
+        }
+        viewModel.deathRegionOne.observe(viewLifecycleOwner){
+            binding.deathRegionField.reset()
+        }
+        viewModel.deathRegionTwo.observe(viewLifecycleOwner){
+            binding.deathRegion2.reset()
+        }
+        lifecycleScope.launch {
+            viewModel.fieldError.collect {
+                when(it.first){
+                    "deathHours" -> {
+                        binding.deathHours.validate(requireContext(),it.second)
+                    }
+                    "region1" -> {
+                        binding.deathRegionField.validate(requireContext(),it.second)
+                    }
+                    "reason1" -> {
+                        binding.deathReason.validate(requireContext(),it.second)
+                    }
+                    "region2" -> {
+                        binding.deathRegion2.validate(requireContext(),it.second)
+                    }
+                    "reason2" -> {
+                        binding.deathReason2.validate(requireContext(),it.second)
+                    }
+                }
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/example/patient/screens/death/RegisterDeathViewModel.kt
+++ app/src/main/java/com/example/patient/screens/death/RegisterDeathViewModel.kt
@@ -1,10 +1,95 @@
 package com.example.patient.screens.death
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.viewModelScope
 import com.example.patient.utils.base.BaseViewModel
+import com.example.patient.utils.enums.InputErrorType
+import com.example.patient.utils.ui.Validator.validateTextFields
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@FlowPreview
 @HiltViewModel
 class RegisterDeathViewModel @Inject constructor() : BaseViewModel() {
 
+    val deathRegionOne=MutableLiveData("")
+    val deathReasonOne=MutableLiveData("")
+    val deathHours=MutableLiveData("")
+    val deathRegionTwo=MutableLiveData("")
+    val deathReasonTwo=MutableLiveData("")
+
+    val buttonEnabled = MutableLiveData(false)
+    private val numberOfValidFields = MutableLiveData(2)
+    val fieldError = MutableSharedFlow<Pair<String, InputErrorType>>()
+    private val listOfFields = mutableListOf<Pair<String, InputErrorType>>()
+
+    private fun addField(field: Pair<String, InputErrorType>) {
+        exists(field)
+        listOfFields.add(field)
+        enableButton()
+    }
+
+    private fun exists(field: Pair<String, InputErrorType>) {
+        listOfFields.remove(field)
+    }
+    fun setNumberOfFields(number:Int){
+        numberOfValidFields.postValue(number)
+    }
+
+    private fun enableButton() {
+        val validFields = listOfFields.filter { pair -> pair.second == InputErrorType.VALID }
+        buttonEnabled.postValue(validFields.size == numberOfValidFields.value)
+    }
+    fun validateDeathReasonOne(){
+        viewModelScope.launch {
+            deathReasonOne.asFlow().debounce(300).distinctUntilChanged().collect {
+                val err=validateTextFields("reason1",it)
+                addField(err)
+                fieldError.emit(err)
+            }
+        }
+    }
+    fun validateDeathRegionOne(){
+        viewModelScope.launch {
+            deathReasonOne.asFlow().debounce(300).distinctUntilChanged().collect {
+                val err=validateTextFields("region1",it)
+                addField(err)
+                fieldError.emit(err)
+            }
+        }
+    }
+    fun validateDeathHours(){
+        viewModelScope.launch {
+            deathReasonOne.asFlow().debounce(300).distinctUntilChanged().collect {
+                val err=validateTextFields("deathHours",it)
+                addField(err)
+                fieldError.emit(err)
+            }
+        }
+    }
+    fun validateDeathReasonTwo(){
+        viewModelScope.launch {
+            deathReasonOne.asFlow().debounce(300).distinctUntilChanged().collect {
+                val err=validateTextFields("reason2",it)
+                addField(err)
+                fieldError.emit(err)
+            }
+        }
+    }
+    fun validateDeathRegionTwo(){
+        viewModelScope.launch {
+            deathReasonOne.asFlow().debounce(300).distinctUntilChanged().collect {
+                val err=validateTextFields("region2",it)
+                addField(err)
+                fieldError.emit(err)
+            }
+        }
+    }
 }
